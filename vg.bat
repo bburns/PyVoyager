432ce3f60170eaa00ef76d3610d42e193ab7aa17
Brian Burns <bburns.km@gmail.com>
2016-07-10 10:16:40 -0500

    move db and test folders to top level - inprogress

--- vg.bat
+++ vg.bat
@@ -4,5 +4,5 @@
 :: note: %~dp0 gives you the drive and path of the current batch file,
 :: so you can refer to other files in this same folder.
 
-@python %~dp0\vg.py %*
+@python %~dp0\src\vg.py %*
 

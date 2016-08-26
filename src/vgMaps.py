5444b231458973695bd7d775898e8018f3fb7926
Brian Burns <bburns.km@gmail.com>
2016-08-26 17:07:10 -0500

    vg maps - inprogress

--- src/vgMaps.py
+++ src/vgMaps.py
@@ -46,13 +46,19 @@ def vgMaps():
     abberationCorrection = 'NONE'
     
     
+    #. loop through these, save each file to stepxx_maps/map-Jupiter-Voyager1.jpg etc
+    #. each should have azimuth and elevation
+    #. crop each file when done to a square
+    #. vg titles could use these for titlepage for each system flyby
+    #. use futura font
+    
     # Voyager 1
     
     bodies = ['Jupiter', 'Voyager 1', 'Io', 'Europa', 'Ganymede', 'Callisto']
     utcClosest = "1979-03-05"
     ndays = 5
     
-    # bodies = ['Saturn','Voyager 1','Titan','Enceladus','Rhea']
+    # bodies = ['Saturn','Voyager 1','Titan','Enceladus','Rhea','Mimas','Tethys','Dione']
     # utcClosest = "1980-11-12"
     # ndays = 3
     
@@ -62,30 +68,27 @@ def vgMaps():
     # utcClosest = "1979-07-09"
     # ndays = 5
     
-    # bodies = ['Saturn','Voyager 2','Titan','Enceladus','Rhea']
+    # bodies = ['Saturn','Voyager 2','Titan','Enceladus','Rhea','Mimas','Tethys','Dione']
     # utcClosest = "1981-08-25"
     # ndays = 3
     
-    # bodies = ['Uranus','Voyager 2','Ariel','Miranda']
+    # bodies = ['Uranus','Voyager 2','Ariel','Miranda','Oberon','Titania','Umbriel']
     # utcClosest = "1986-01-24"
     # ndays = 3
     
     # bodies = ['Neptune','Voyager 2','Triton']
     # utcClosest = "1989-08-25"
-    # ndays = 3
-    
-    nsteps = 100
+    # ndays = 1
     
     planet = bodies[0]
     observer = bodies[1]
     
+    nsteps = 100 # plot density
     
-    # get ephemeris time (seconds since J2000)
+    # get ephemeris time around closest approach (seconds since J2000)
     etClosest = int(spice.str2et(utcClosest))
     etStart = int(etClosest - ndays * 24*60*60 / 2)
     etEnd = int(etClosest + ndays * 24*60*60 / 2)
-    # etStart = int(spice.str2et(utcStart))
-    # etEnd = int(spice.str2et(utcEnd))
     etStep = int((etEnd - etStart) / nsteps)
     
     ets = []
@@ -96,24 +99,26 @@ def vgMaps():
     for body in bodies:
         minDist[body] = 9e15
         
-        
+    # loop over time range, get positions
     for et in xrange(etStart, etEnd, etStep):
-        # get position of observer (eg Voyager 1) relative to planet (eg Jupiter).
-        # position is an (x,y,z) coordinate in the given frame of reference.
         row = []
         for body in bodies:
-            position, lightTime = spice.spkpos(planet, et, frame,
-                                                      # abberationCorrection, observer)
-                                                      abberationCorrection, body)
+            # get position of body (voyager or moon) relative to planet (eg Jupiter).
+            # position is an (x,y,z) coordinate in the given frame of reference.
+            position, lightTime = spice.spkpos(planet, et, frame, abberationCorrection, body)
 
+            # save time and position to arrays
             ets.append(et)
-            # positionsPlanet.append([0,0,0])
-            # positionsVoyage.append(positionVoyager)
             row.append(position)
-            if body==bodies[1]: # craft
-                posVoyager = position
-            # elif body=='Jupiter':
-            elif body==bodies[0]: # planet
+            
+            # find closest approach of voyager to each body
+            if body==observer: # voyager
+                posVoyager = position # save for other bodies
+                distance = int(libspice.getDistance(position))
+                if distance < minDist[body]:
+                    minDist[body] = distance
+                    minPos[body] = position
+            elif body==planet:
                 pass
             else:
                 # get distance to voyager, km
@@ -123,11 +128,6 @@ def vgMaps():
                     minDist[body] = distance
                     minPos[body] = position
         positions.append(row)
-
-
-        # print position
-        # print distance
-        # print et, position, distance
         
     # Clean up the kernels
     spice.kclear()
@@ -143,63 +143,107 @@ def vgMaps():
     mpl.rcParams['legend.fontsize'] = 10
 
     fig = plt.figure()
-    # ax = fig.gca(projection='3d',axisbg='black')
-    ax = fig.gca(projection='3d',axisbg='0.1')
-    colors = ['r','r','g','b','y','c','m']
-    # colors = ['r','w','#40ff50','#4050ff','y','#ffff00','c']
-    i = 0
-    # dots = {}
+    ax = fig.gca(projection='3d',axisbg='0.05')
+    
+    # title = observer + ' at ' + planet
+    # ax.set_title(title,color='w')
+    
+    # colors = ['r','r','g','b','y','c','m']
+    # colors = ['r','g','0.7','0.7','0.7','0.7','0.7']
+    colors = ['r','g','b','b','b','b','b','b','b']
+    
     dots = []
     dotlabels = []
+    
+    # draw planet
+    color = colors[0]
+    dot, = ax.plot([0],[0],[0],color+'o',label=bodies[0])
+    dots.append(dot)
+    dotlabels.append(bodies[0])
+    
+    i = 0
     for body in bodies:
         rows = [row[i] for row in positions]
         x = [row[0] for row in rows]
         y = [row[1] for row in rows]
         z = [row[2] for row in rows]
-        ax.plot(x, y, z, '0.2', label=body)
+        linestyle = 'dotted' if body==observer else 'solid'
+        ax.plot(x, y, z, color='0.3', linestyle=linestyle, label=body)
         
-        # draw dot at closest approach
+        # draw a dot for moon at closest approach
         try:
             pos = minPos[body]
             x = [pos[0]]
             y = [pos[1]]
             z = [pos[2]]
             color = colors[i]
-            # ax.plot(x,y,z,color+'o')
-            dot, = ax.plot(x,y,z,color+'o',label=body)
+            dot, = ax.plot(x,y,z,color=color, marker='o',label=body)
             dots.append(dot)
             dotlabels.append(body)
         except:
             pass
-
+        
         i += 1
 
     # add legend
-    # plt.legend(dots, bodies)
-    plt.legend(dots, dotlabels)
+    # eh
+    # plt.legend(dots, dotlabels, numpoints=1)
         
-    # draw planet
-    ax.plot([0],[0],[0],'ro')
-    
     # label axes
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
+    # ax.set_xlabel('x')
+    # ax.set_ylabel('y')
+    # ax.set_zlabel('z')
     
     # make it a cube
-    lim = 1e6
+    lim = 1e6 # 1 million km
     ax.set_xlim([-lim,lim])
     ax.set_ylim([-lim,lim])
     ax.set_zlim([-lim,lim])
     
-    # ax.legend()
-
+    # label planet, voyager, moons
+    offset = int(2 * lim / 100)
+    fontsize = 12
+    ax.text(offset,offset,offset,planet,size=fontsize,color='w')
+    for key in minPos:
+        pos = minPos[key]
+        x = pos[0]
+        y = pos[1]
+        z = pos[2]
+        ax.text(x+offset,y+offset,z+offset,key,size=fontsize,color='w')
+    
+    # draw an arrow at end of voyager's trajectory to indicate direction
+    # from stackoverflow
+    from matplotlib.patches import FancyArrowPatch
+    from mpl_toolkits.mplot3d import proj3d
+    class Arrow3D(FancyArrowPatch):
+        def __init__(self, xs, ys, zs, *args, **kwargs):
+            FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
+            self._verts3d = xs, ys, zs
+        def draw(self, renderer):
+            xs3d, ys3d, zs3d = self._verts3d
+            xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
+            self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
+            FancyArrowPatch.draw(self, renderer)
+    i = 1 # voyager
+    rows = [row[i] for row in positions]
+    x = [row[0] for row in rows]
+    y = [row[1] for row in rows]
+    z = [row[2] for row in rows]
+    dx=[x[-2],x[-1]]
+    dy=[y[-2],y[-1]]
+    dz=[z[-2],z[-1]]
+    a = Arrow3D(dx,dy,dz, mutation_scale=20, lw=1, arrowstyle="-|>", color="w")
+    ax.add_artist(a)
+    
+    #. each system will have its own
+    ax.view_init(azim=-23, elev=56)
+    
     plt.axis('off')
     
     # save image without white border
     # see http://stackoverflow.com/questions/11837979/
     #     removing-white-space-around-a-saved-image-in-matplotlib
-    plt.savefig("map.png", bbox_inches='tight', pad_inches=0.0)
+    # plt.savefig("map.png", bbox_inches='tight', pad_inches=0.0)
     
     plt.show()
     

6479ef9acda42527ed08f6532e269f81cd0dc42d
Brian Burns <bburns.km@gmail.com>
2022-05-09 12:04:15 -0500

    cmt

--- vg.bat
+++ vg.bat
@@ -1,8 +1,6 @@
-
 :: launch vg.py command handler
 
 :: note: %~dp0 gives you the drive and path of the current batch file,
 :: so you can refer to other files in this same folder.
 :: -tt throws an error if it finds tabs mixed with spaces
 @python -tt %~dp0\src\vg.py %*
-

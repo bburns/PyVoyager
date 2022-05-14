852f4dd051a4326a13ff7eabaa0d44add51c1c84
Brian Burns <bburns.km@gmail.com>
2022-05-14 09:46:34 -0500

    rm bat

--- @backup_to_gdrive.bat
+++
@@ -1,5 +0,0 @@
-
-:: backup all files excluding the data directory to dropbox
-:: also exclude the SPICE .bsp files, which are huge
-:: robocopy . c:\projects\voyager\backup /mir /xd data /xf *.bsp
-robocopy . c:\users\bburns\gdrive\projects\voyager\backup /mir /xd data /xf *.bsp
